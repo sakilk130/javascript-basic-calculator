e7f3753c8b59053c691db8603375f03565ec6882
sakilk130 <sakilk130@gmail.com>
2020-09-19 01:33:24 +0600

    fix clear and point

--- script.js
+++ script.js
@@ -9,12 +9,22 @@ function numberValue(number) {
     displayValue === '0' ? number : displayValue + number;
 }
 
+function addDecimal() {
+  if (!calculatorDisplay.textContent.includes('.')) {
+    calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
+  }
+}
+
 inputBtns.forEach((inputBtn) => {
   if (inputBtn.classList.length === 0) {
     inputBtn.addEventListener('click', () => numberValue(inputBtn.value));
   } else if (inputBtn.classList.contains('operator')) {
     inputBtn.addEventListener('click', () => numberValue(inputBtn.value));
   } else if (inputBtn.classList.contains('decimal')) {
-    inputBtn.addEventListener('click', () => numberValue(inputBtn.value));
+    inputBtn.addEventListener('click', () => addDecimal());
   }
 });
+function clearAll() {
+  calculatorDisplay.textContent = '0';
+}
+clearBtn.addEventListener('click', clearAll);
